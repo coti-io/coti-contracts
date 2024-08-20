39d42eba90e4919e4fecb6f907f1b8955c090678
Spencer Miller <spencer.github.123@gmail.com>
2024-08-20 13:23:06 +0300

    add basic contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-};
+  defaultNetwork: "devnet",
+  solidity: "0.8.20",
+  networks: {
+    devnet: {
+      url: "https://devnet.coti.io/rpc",
+      chainId: 13068200,
+    },
+  }
+}
 
 export default config;
