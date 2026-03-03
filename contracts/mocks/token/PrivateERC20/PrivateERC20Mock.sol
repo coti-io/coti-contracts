c5de6781f8ba63e9543ebd271fad236a6c638b55
Mesika Guy <guy@coti.io>
2026-03-03 22:16:38 +0200

    you cant override the mint . in PrivateERC20 the amount is a clear value input and not gt type you cant override burn for the same issue

--- contracts/mocks/token/PrivateERC20/PrivateERC20Mock.sol
+++ contracts/mocks/token/PrivateERC20/PrivateERC20Mock.sol
@@ -7,11 +7,4 @@ import "../../../token/PrivateERC20/PrivateERC20.sol";
 contract PrivateERC20Mock is PrivateERC20 {
     constructor() PrivateERC20("PrivateERC20Mock", "PE20M") {}
 
-    function mint(address account, uint256 amount) external {
-        _mint(account, MpcCore.setPublic256(amount));
-    }
-
-    function burn(address account, uint256 amount) external {
-        _burn(account, MpcCore.setPublic256(amount));
-    }
 }
